dcf6d97208422f802ce4f1b4c2dab0ef771f217f
Hiroshi Saito <1984.saitoh@gmail.com>
2013-11-09 05:55:46 +0900

    rspec追記

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Project do
 
-  context 'ファーストテスト' do
+  context 'validation' do
     it '正常の保存できること' do
       @project = FactoryGirl.build(:project)
       expect(@project.save).to be_true
@@ -11,6 +11,16 @@ describe Project do
     it 'プロジェクトが１つ以上であること' do
       FactoryGirl.create(:project)
       expect(Project.count).to be_>= 1
+    end
+
+    it 'プロジェクトタイトルが3桁未満は登録出来ないこと' do
+      @project = FactoryGirl.build(:project, title: "プロ")
+      expect(@project.save).to be_false
+    end
+
+    it 'プロジェクトタイトルが3桁以上は登録出来ること' do
+      @project = FactoryGirl.build(:project, title: "プロジェクトOK")
+      expect(@project.save).to be_true
     end
 
   end
